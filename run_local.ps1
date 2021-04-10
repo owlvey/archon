ebc50c62858d345511b862c92f265a89da613181
Gregory Valderrama <gcvalderrama@hotmail.com>
2021-04-09 23:05:52 -0500

    add

--- run_local.ps1
+++ run_local.ps1
@@ -1,9 +1,10 @@
 docker stop owlvey-archon
 docker rm owlvey-archon
+#
 docker run --name=owlvey-archon `
-    --mount type=bind,source="/workspace/gregory/owlvey/archon/system_docker.yaml",target=/workspace/config.yaml  `
     --mount type=bind,source="/workspace/gregory/owlvey/metadata",target="/workspace/metadata"  `
     --mount type=bind,source="/workspace/gregory/owlvey/data",target="/workspace/data"  `
+    --mount type=bind,source="/workspace/gregory/owlvey/archon/system_docker.yaml",target=/workspace/config.yaml  `
     -e "OWLVEY_CONFIG=/workspace/config.yaml"`
-    -e "OWLVEY_LOGGING=WARNING"`
+    -e "OWLVEY_LOGGING=WARNING"`zzz
     owlvey-archon:latest
